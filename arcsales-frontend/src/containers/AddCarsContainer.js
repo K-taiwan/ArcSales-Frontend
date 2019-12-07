f314e02b8051133a603b5d48e01d1241793648d2
KennyK-TW <b2khakashu@yahoo.com>
2019-12-07 15:08:14 -0800

    was able to get user to Car list

--- arcsales-frontend/src/containers/AddCarsContainer.js
+++ arcsales-frontend/src/containers/AddCarsContainer.js
@@ -19,31 +19,25 @@ class AddCarsContainer extends Component {
          })
          .catch((err) => console.log(err));
 
-         axios.get(`${process.env.REACT_APP_API_URL}/users/${userId}`, {
-            withCredentials: true,
-        })
-            .then(res => console.log(res))
-            .catch(err => console.log(err))
         }
     
         
       // GET USER CARS
 
 
-    // componentDidUpdate(){
-    //     const userId = localStorage.getItem('uid');
-    //     // GET USER PROFILE
-    //     axios.get(`${process.env.REACT_APP_API_URL}/cars/create/${userId}`, {
-    //         withCredentials: true,
-    //     })
-    //         .then((res) => {
-    //             this.setState({
-    //                 car: res.data.data
-    //             });
-    //         })
-    //         .catch((err) => console.log(err));
-           
-    // }
+    componentDidUpdate(){
+        const userId = localStorage.getItem('uid');
+        axios.get(`${process.env.REACT_APP_API_URL}/cars/${userId}`,{
+            withCredentials: true,
+        })
+         .then((res) => {
+             this.setState({
+                car: res.data.data,
+             });
+         })
+         .catch((err) => console.log(err));
+
+        }
 
     
 
